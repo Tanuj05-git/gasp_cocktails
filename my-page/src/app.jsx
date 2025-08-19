cc04557ce9303ab81ac553bb4ee1872e80e6c3cc
Your Name <your-email@example.com>
2025-08-19 06:03:59 +0530

    implement the hero section

---
+++ my-page/src/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
